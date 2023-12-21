a0344350b5a6cc130eec48ebbd42997a21cbe6ca
Amnaqasmi <drakshanaltaf@gmail.com>
2023-12-20 23:39:07 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+var n = prompt('How many more times? ');
+console.log(n);
